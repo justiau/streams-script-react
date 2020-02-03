f8f4d73d41dc7e4a6f323d5e2aa7057f5904a4a6
Justin Au <u6015857@anu.edu.au>
2020-02-03 14:01:11 +1100

    Changing text and moving images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
-import luffy from './images/luffy-face.jpg';
-import deku from './images/deku-face.jpg';
-import naruto from './images/naruto-face.jpg';
-import eleven11 from './images/taeyeon.jpg';
+import taeyeon from './images/taeyeon.jpg';
 import blanc from './images/blanc.jpg';
 import cidergirl from './images/cidergirl.jpg';
 import tenki_no_ko from './images/tenki-no-ko.jpg';
@@ -16,7 +13,7 @@ request.send();
 
 const script = request.responseText;
 const text = "STREAMS Rego Script"
-const images = [eleven11,blanc,cidergirl,tenki_no_ko,all_about_you,breathe]
+const images = [blanc,all_about_you,breathe,taeyeon,cidergirl,tenki_no_ko]
 const imgStyle = {
 	maxWidth: 260,
 	maxHeight: 260,
@@ -58,13 +55,17 @@ class Bookmarklet extends React.Component {
 
 class App extends React.Component {
 	render() {
-		const startingIndex = 1;
+		const startingIndex = 0;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1>ANU STREAMS Automatic Tutorial Registration</h1>
 				</header>
 				<body className="App-body">
+					<div id="setup">
+						<h2>Setup</h2>
+						<p>Click and drag any of the below images or link into your bookmarks.</p> 
+					</div>
 					<div id="bookmarklets">
 						<Bookmarklet index={startingIndex} text={text}/>
 						<Bookmarklet index={startingIndex} text={text}/>
@@ -76,25 +77,23 @@ class App extends React.Component {
 						<br/>
 						<Bookmarklet text={text}/>
 					</div>
-					<div id="setup">
-						<h2>Setup</h2>
-						<p>Simply click and drag any of the above images or the link above into your bookmarks.</p> 
-					</div>
 					<div id="usage">
 						<h2>Usage</h2>
 						<ol>
-							<li>First login at <a href="https://cs.anu.edu.au/streams/index.php">streams</a></li>
-							<li>Then click on the <i>'Streams Rego Script'</i> Bookmark</li>
+							<li>First login at <a href="https://cs.anu.edu.au/streams/index.php" target="_blank" rel="noopener noreferrer">streams</a></li>
+							<li>Then click on the <i>'Streams Rego Script'</i> bookmark</li>
 							<li>Fill in the details with your preferences and click <i>'Start'</i></li>
 						</ol>
+					</div>
+					<div id="notes">
 						<p style={{"fontWeight":"bold"}}>
 							Note: The page is not meant to refresh
 						</p>
 						<p>
-							This script uses Axios to continually check the server code so there is no need to refresh the page itself.<br/>Once the tutorial is available, the script will click the Register button, and the page will then change.
+							This script directly and continually checks the server without refreshing the page itself.<br/>Once the tutorial is available, the script will click the Register button, and then the page will reload.
 						</p>
+						<p>By Justin Au</p>
 					</div>
-					<p>By Justin Au</p>
 				</body>
 			</div>
 		);
